2e62edb2d3ef3f2d4be56f05deb2f4cd3dc875a7
Bob Kubista <bobbykubista@yahoo.com>
2015-09-09 12:00:25 +0200

    added localeAdapter

---
+++ utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/domainmodel/adapter/LocaleAdapter.java
@@ -0,0 +1,39 @@
+/**
+ *
+ */
+package bobkubista.examples.utils.domain.model.domainmodel.adapter;
+
+import java.util.Locale;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * {@link XmlAdapter} for {@link String} to {@link Locale} Can be used with
+ * {@link javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter}<P> Examlpe:<P>
+ * <code> @XmlJavaTypeAdapter(LocaleAdapter.class)<BR>private Locale locale;</
+ * code>
+ *
+ * @author bkubista
+ *
+ */
+public class LocaleAdapter extends XmlAdapter<String, Locale> {
+
+	@Override
+	public String marshal(final Locale v) throws Exception {
+		String result = null;
+		if (v != null) {
+			result = v.getLanguage();
+		}
+		return result;
+	}
+
+	@Override
+	public Locale unmarshal(final String v) throws Exception {
+		Locale result = null;
+		if (v != null) {
+			result = new Locale(v);
+		}
+		return result;
+	}
+
+}
