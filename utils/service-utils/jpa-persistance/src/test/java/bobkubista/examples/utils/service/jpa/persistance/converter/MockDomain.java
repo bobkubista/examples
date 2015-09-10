9785e139c746a4d3633cd8bdec46ee7d550677dc
Bob Kubista <bobbykubista@yahoo.com>
2015-09-10 09:53:23 +0200

    added unit tests

---
+++ utils/service-utils/jpa-persistance/src/test/java/bobkubista/examples/utils/service/jpa/persistance/converter/MockDomain.java
@@ -0,0 +1,44 @@
+package bobkubista.examples.utils.service.jpa.persistance.converter;
+
+import bobkubista.examples.utils.domain.model.domainmodel.identification.ActiveDomainObject;
+
+public class MockDomain extends ActiveDomainObject<Long> {
+
+	private static final long serialVersionUID = -3472971306874956522L;
+	private boolean active;
+	private String functionalId;
+	private Long id;
+
+	@Override
+	public String getFunctionalId() {
+		return this.functionalId;
+	}
+
+	@Override
+
+	public Long getId() {
+		return this.id;
+	}
+
+	@Override
+	public boolean isActive() {
+		return this.active;
+	}
+
+	@Override
+	public void setActive(final boolean active) {
+		this.active = active;
+	}
+
+	@Override
+
+	public void setFunctionalId(final String functionalId) {
+		this.functionalId = functionalId;
+	}
+
+	@Override
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+}
---
+++ utils/service-utils/jpa-persistance/src/test/java/bobkubista/examples/utils/service/jpa/persistance/converter/MockEntity.java
@@ -0,0 +1,41 @@
+package bobkubista.examples.utils.service.jpa.persistance.converter;
+
+import bobkubista.examples.utils.service.jpa.persistance.entity.ActiveEntity;
+
+public class MockEntity extends ActiveEntity<Long> {
+
+	private static final long serialVersionUID = -1029339275802750276L;
+	private boolean active;
+	private String functionalId;
+	private Long id;
+
+	@Override
+	public String getFunctionalId() {
+		return this.functionalId;
+	}
+
+	@Override
+	public Long getId() {
+		return this.id;
+	}
+
+	@Override
+	public boolean isActive() {
+		return this.active;
+	}
+
+	@Override
+	public void setActive(final boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public void setFunctionalId(final String functionalId) {
+		this.functionalId = functionalId;
+	}
+
+	@Override
+	public void setId(final Long id) {
+		this.id = id;
+	}
+}
