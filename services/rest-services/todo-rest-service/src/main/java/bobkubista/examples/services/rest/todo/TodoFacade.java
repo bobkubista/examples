a373832b131b067c6b4ca817fcbc90136c69187a
Bob Kubista <bobbykubista@yahoo.com>
2015-09-11 11:31:34 +0200

    added logging interceptor binding

--- services/rest-services/todo-rest-service/src/main/java/bobkubista/examples/services/rest/todo/TodoFacade.java
+++ services/rest-services/todo-rest-service/src/main/java/bobkubista/examples/services/rest/todo/TodoFacade.java
@@ -6,9 +6,11 @@ import javax.ws.rs.Path;
 import bobkubista.examples.services.api.todo.TodoApi;
 import bobkubista.examples.services.api.todo.domain.TodoList;
 import bobkubista.examples.services.api.todo.domain.TodoListCollection;
+import bobkubista.examples.utils.jee.annotation.Logging;
 import bobkubista.examples.utils.service.jpa.persistance.facade.GenericActiveFacade;
 
 @Path("/")
+@Logging
 public class TodoFacade extends GenericActiveFacade<TodoList, Long, TodoListEntity, TodoListCollection>implements TodoApi {
 
 	@Inject
