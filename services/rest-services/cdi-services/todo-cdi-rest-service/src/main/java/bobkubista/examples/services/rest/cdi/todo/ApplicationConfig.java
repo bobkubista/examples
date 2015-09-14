db90fc5b74d33e1391afc2bf4d222f06ba10e89e
Bob Kubista <bobbykubista@yahoo.com>
2015-09-14 16:34:13 +0200

    added application config

---
+++ services/rest-services/cdi-services/todo-cdi-rest-service/src/main/java/bobkubista/examples/services/rest/cdi/todo/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package bobkubista.examples.services.rest.cdi.todo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class ApplicationConfig extends Application {
+
+}
