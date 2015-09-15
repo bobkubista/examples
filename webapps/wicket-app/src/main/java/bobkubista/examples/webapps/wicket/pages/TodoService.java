97ae238e44f3603522f62350bb727355cd11dc17
Bob Kubista <bobbykubista@yahoo.com>
2015-09-15 16:33:24 +0200

    removed unused type

--- webapps/wicket-app/src/main/java/bobkubista/examples/webapps/wicket/pages/TodoService.java
+++ webapps/wicket-app/src/main/java/bobkubista/examples/webapps/wicket/pages/TodoService.java
@@ -4,13 +4,12 @@
 package bobkubista.examples.webapps.wicket.pages;
 
 import bobkubista.examples.services.api.todo.domain.TodoList;
-import bobkubista.examples.services.api.todo.domain.TodoListCollection;
 import bobkubista.examples.utils.rest.utils.service.ActiveService;
 
 /**
  * @author Bob Kubista
  *
  */
-public interface TodoService extends ActiveService<TodoList, Long, TodoListCollection> {
+public interface TodoService extends ActiveService<TodoList, Long> {
 
 }
