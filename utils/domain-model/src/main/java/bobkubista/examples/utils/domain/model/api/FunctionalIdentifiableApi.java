23a16a37b39b18dfa53c81cf6ce58346552412d7
Bob Kubista <bobbykubista@yahoo.com>
2015-09-15 15:05:10 +0200

    fixed javadoc

--- utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/api/FunctionalIdentifiableApi.java
+++ utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/api/FunctionalIdentifiableApi.java
@@ -10,19 +10,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import bobkubista.examples.utils.domain.model.domainmodel.identification.DomainObject;
 import bobkubista.examples.utils.domain.model.domainmodel.identification.FunctionalIdentifiableDomainObject;
 
 /**
- * @author bkubista @param <DMO> {@link
- * FunctionalIdentifiableDomainObject} @param <ID> the identifier of the {@link
- * FunctionalIdentifiableDomainObject}
+ * @author bkubista
+ * @param <DMO>
+ *            {@link FunctionalIdentifiableDomainObject}
+ * @param <ID>
+ *            the identifier of the {@link FunctionalIdentifiableDomainObject}
+ * 
  */
 public interface FunctionalIdentifiableApi<DMO extends FunctionalIdentifiableDomainObject<ID>, ID extends Serializable> extends IdentifiableApi<DMO, ID> {
 
 	/**
 	 * get the <code>DMO</code>
 	 *
-	 * @param identifier the identfier @return the <code>DMO</code>
+	 * @param identifier
+	 *            the identfier @return the <code>DMO</code>
 	 */
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
@@ -35,7 +40,8 @@ public interface FunctionalIdentifiableApi<DMO extends FunctionalIdentifiableDom
 	/**
 	 * Get the identifier that goes with the functional id
 	 *
-	 * @param fId functional id @return the <code>ID</code>
+	 * @param fId
+	 *            functional id @return the <code>ID</code>
 	 */
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
@@ -48,7 +54,8 @@ public interface FunctionalIdentifiableApi<DMO extends FunctionalIdentifiableDom
 	/**
 	 * get the {@link DomainObject}
 	 *
-	 * @param identifier the identfier @return the {@link DomainObject}
+	 * @param identifier
+	 *            the identfier @return the {@link DomainObject}
 	 */
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
