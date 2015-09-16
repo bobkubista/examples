9a3937472554e4f625e3714e3b91dda72db018ce
Bob Kubista <bobbykubista@yahoo.com>
2015-09-16 14:07:51 +0200

    remove whiteline

--- utils/test-utils/src/main/java/bobkubista/examples/utils/service/jpa/persistance/AbstractIdentifiableDaoIT.java
+++ utils/test-utils/src/main/java/bobkubista/examples/utils/service/jpa/persistance/AbstractIdentifiableDaoIT.java
@@ -118,5 +118,4 @@ public abstract class AbstractIdentifiableDaoIT<TYPE extends AbstractIdentifiabl
      *            update some fields for updating
      */
     protected abstract void updateEntity(TYPE entity);
-
 }
