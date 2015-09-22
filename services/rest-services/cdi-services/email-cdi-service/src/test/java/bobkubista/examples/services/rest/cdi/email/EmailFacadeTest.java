c274d0e4c3b1feee8c33d9776f1ccd30e6ce9c55
Bob Kubista <bobbykubista@yahoo.com>
2015-09-22 13:33:01 +0200

    added facade test

---
+++ services/rest-services/cdi-services/email-cdi-service/src/test/java/bobkubista/examples/services/rest/cdi/email/EmailFacadeTest.java
@@ -0,0 +1,52 @@
+/**
+ *
+ */
+package bobkubista.examples.services.rest.cdi.email;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.dumbster.smtp.SimpleSmtpServer;
+
+import bobkubista.example.utils.property.ServerProperties;
+import bobkubista.examples.services.api.email.model.EmailContext;
+import bobkubista.examples.services.api.email.model.EmailContext.EmailBuilder;
+
+/**
+ * @author Bob
+ *
+ */
+public class EmailFacadeTest {
+
+    private static SimpleSmtpServer server;
+    private final EmailFacade facade = new EmailFacade();
+
+    @After
+    public void afterClass() {
+        server.stop();
+    }
+
+    @Before
+    public void beforeClass() {
+        server = SimpleSmtpServer.start(Integer.valueOf(ServerProperties.getString("email.smtp.port")));
+    }
+
+    /**
+     * Test method for
+     * {@link bobkubista.examples.services.rest.cdi.email.EmailFacade#sendEmail(bobkubista.examples.services.api.email.model.EmailContext)}
+     * .
+     */
+    @Test
+    public void testSendEmail() {
+        final EmailContext context = new EmailBuilder("bla@foo.bar", "foobar").build();
+        final Response result = this.facade.sendEmail(context);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(200, result.getStatus());
+    }
+
+}
