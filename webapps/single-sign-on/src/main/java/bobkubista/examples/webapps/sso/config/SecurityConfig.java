54b7a902abd3839c7d19dc4fd4aa02787381fa33
Bob Kubista <bobbykubista@yahoo.com>
2015-09-26 01:46:09 +0200

    single sign on

---
+++ webapps/single-sign-on/src/main/java/bobkubista/examples/webapps/sso/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+// package bobkubista.examples.webapps.sso.config;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import
+// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import
+// org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+// @Autowired
+// public void configureGlobal(final AuthenticationManagerBuilder auth) throws
+// Exception {
+// auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+// }
+// }
