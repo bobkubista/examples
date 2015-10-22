5a4dbd760f3f5100b2247c6d6946af05fc06266c
Bob Kubista <bkubista@first8.nl>
2015-10-22 16:54:57 +0200

    api change

--- services/api/user-service-api/src/main/java/bobkubista/examples/services/api/user/UserApi.java
+++ services/api/user-service-api/src/main/java/bobkubista/examples/services/api/user/UserApi.java
@@ -1,7 +1,13 @@
 package bobkubista.examples.services.api.user;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
 import bobkubista.examples.services.api.user.domain.User;
 import bobkubista.examples.utils.domain.model.api.ActiveApi;
+import bobkubista.examples.utils.domain.model.api.IdentifiableApi;
 
 /**
  * @author Bob Kubista
@@ -9,4 +15,18 @@ import bobkubista.examples.utils.domain.model.api.ActiveApi;
  */
 public interface UserApi extends ActiveApi<User, Long> {
 
+    /**
+     * Checks if the user is authorized for a given right.
+     *
+     * @param userId
+     *            the identifier of the user
+     * @param right
+     *            the name of the right
+     * @return Status 200 if authorized, otherwise 401
+     */
+    @GET
+    @Path("{userId}/{right}")
+    default Response isAuthorized(@PathParam("userId") final Long userId, @PathParam("right") final String right) {
+        return IdentifiableApi.buildNotImplementedResponse(null);
+    }
 }
