3b0767fc8d3b6c8fae9198558243d591d5277b67
Bob Kubista <bobbykubista@yahoo.com>
2015-10-25 13:05:56 +0100

    properties rework

--- utils/property-utils/src/main/java/bobkubista/example/utils/property/ServerProperties.java
+++ utils/property-utils/src/main/java/bobkubista/example/utils/property/ServerProperties.java
@@ -21,85 +21,83 @@ import org.slf4j.LoggerFactory;
  *
  */
 public enum ServerProperties {
-    INSTANCE;
-    private static final Logger LOGGER = LoggerFactory.getLogger(ServerProperties.class);
+	INSTANCE;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServerProperties.class);
 
-    private static Properties props;
+	private static Properties props;
 
-    private static final String SERVER_PROP_FILE = "server.properties";
+	private static final String SERVER_PROP_FILE = "server.properties";
 
-    static {
-        if (props == null) {
-            InputStream serverPropLocation = null;
-            File configFile;
-            try {
-                try {
-                    LOGGER.info("Getting resource file location from JDNI");
-                    String propFolder;
-                    // http://stackoverflow.com/questions/13956651/externalizing-tomcat-webapp-config-from-war-file
+	static {
+		if (props == null) {
+			InputStream serverPropLocation = null;
+			File configFile;
+			try {
+				try {
+					LOGGER.info("Getting resource file location from JDNI");
+					String propFolder;
+					// http://stackoverflow.com/questions/13956651/externalizing-tomcat-webapp-config-from-war-file
 
-                    // get a handle on the JNDI root context
-                    final Context ctx = new InitialContext();
+					// get a handle on the JNDI root context
+					final Context ctx = new InitialContext();
 
-                    // and access the environment variable for this web
-                    // component
-                    propFolder = (String) ctx.lookup("java:comp/env/configurationPath");
-                    configFile = new File(propFolder + ServerProperties.SERVER_PROP_FILE);
-                    serverPropLocation = new FileInputStream(configFile);
-                } catch (NamingException | FileNotFoundException e1) {
+					// and access the environment variable for this web
+					// component
+					propFolder = (String) ctx.lookup("java:comp/env/configurationPath");
+					configFile = new File(propFolder + ServerProperties.SERVER_PROP_FILE);
+					serverPropLocation = new FileInputStream(configFile);
+				} catch (NamingException | FileNotFoundException e1) {
 
-                    LOGGER.warn("Defaulting back to classpath", e1);
-                    LOGGER.debug("Getting resource file location from classpath");
-                    serverPropLocation = Thread.currentThread().getContextClassLoader().getResourceAsStream(ServerProperties.SERVER_PROP_FILE);
-                }
+					LOGGER.warn("Defaulting back to classpath", e1);
+					LOGGER.debug("Getting resource file location from classpath");
+					serverPropLocation = Thread.currentThread().getContextClassLoader().getResourceAsStream(ServerProperties.SERVER_PROP_FILE);
+				}
 
-                loadProperties(serverPropLocation);
-            } finally {
-                if (serverPropLocation != null) {
-                    try {
-                        serverPropLocation.close();
-                    } catch (final IOException e) {
-                        LOGGER.error(e.getMessage(), e);
-                    }
-                }
-            }
-        }
-    }
+				ServerProperties.loadProperties(serverPropLocation);
+			} finally {
+				if (serverPropLocation != null) {
+					try {
+						serverPropLocation.close();
+					} catch (final IOException e) {
+						LOGGER.error(e.getMessage(), e);
+					}
+				}
+			}
+		}
+	}
 
-    /**
-     * Get all properties
-     *
-     * @return
-     */
-    public static Properties getProperies() {
-        return props;
-    }
+	/**
+	 * Get all properties
+	 *
+	 * @return
+	 */
+	public static Properties getProperies() {
+		return props;
+	}
 
-    /**
-     *
-     * @param key
-     *            of the property
-     * @return the property value
-     */
-    public static String getString(final String key) {
-        String result = null;
-        LOGGER.debug("Getting property for key {}", key);
-        result = props.getProperty(key);
-        LOGGER.debug("Got property for key {} and value {}", key, result);
-        return result;
-    }
+	/**
+	 *
+	 * @param key of the property @return the property value
+	 */
+	public static String getString(final String key) {
+		String result = null;
+		LOGGER.debug("Getting property for key {}", key);
+		result = props.getProperty(key);
+		LOGGER.debug("Got property for key {} and value {}", key, result);
+		return result;
+	}
 
-    private static void loadProperties(final InputStream serverPropLocation) {
-        props = new Properties();
-        if (serverPropLocation != null) {
-            try {
-                LOGGER.debug("Loading resource file location from classpath");
-                LOGGER.debug("Loading properties");
-                props.load(serverPropLocation);
-            } catch (final IOException e) {
-                LOGGER.error("Could not load file", e);
-            }
-        }
-    }
+	private static void loadProperties(final InputStream serverPropLocation) {
+		props = new Properties();
+		if (serverPropLocation != null) {
+			try {
+				LOGGER.debug("Loading resource file location from classpath");
+				LOGGER.debug("Loading properties");
+				props.load(serverPropLocation);
+			} catch (final IOException e) {
+				LOGGER.error("Could not load file", e);
+			}
+		}
+	}
 
 }
--- utils/property-utils/src/test/java/bobkubista/example/utils/property/ServerPropertiesTest.java
+++ utils/property-utils/src/test/java/bobkubista/example/utils/property/ServerPropertiesTest.java
@@ -16,23 +16,23 @@ import org.junit.Test;
  */
 public class ServerPropertiesTest {
 
-    @Test
-    public void testGetProperies() throws NamingException {
-        final Properties result = ServerProperties.getProperies();
-        Assert.assertNotNull(result);
-        Assert.assertFalse(result.isEmpty());
-        Assert.assertEquals(1, result.size());
-    }
+	@Test
+	public void testGetProperies() throws NamingException {
+		final Properties result = ServerProperties.getProperies();
+		Assert.assertNotNull(result);
+		Assert.assertFalse(result.isEmpty());
+		Assert.assertEquals(1, result.size());
+	}
 
-    /**
-     * Test method for
-     * {@link bobkubista.example.utils.property.ServerProperties#getString(java.lang.String)}
-     * .
-     */
-    @Test
-    public void testGetString() {
-        final String value = ServerProperties.getString("test");
-        Assert.assertEquals("test1", value);
-    }
+	/**
+	 * Test method for {@link
+	 * bobkubista.example.utils.property.ServerProperties#getString(java.lang.
+	 * String)} .
+	 */
+	@Test
+	public void testGetString() {
+		final String value = ServerProperties.getString("test");
+		Assert.assertEquals("test1", value);
+	}
 
 }
