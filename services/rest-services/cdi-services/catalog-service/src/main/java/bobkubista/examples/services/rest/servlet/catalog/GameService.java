cb1cdf9585c04d65a81a3e8547da7a42f4642d98
Bob Kubista <bkubista@first8.nl>
2015-10-27 12:16:33 +0100

    added transactional

--- services/rest-services/cdi-services/catalog-service/src/main/java/bobkubista/examples/services/rest/servlet/catalog/GameService.java
+++ services/rest-services/cdi-services/catalog-service/src/main/java/bobkubista/examples/services/rest/servlet/catalog/GameService.java
@@ -4,6 +4,7 @@
 package bobkubista.examples.services.rest.servlet.catalog;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import bobkubista.examples.services.rest.servlet.catalog.entity.MainGameEntity;
 import bobkubista.examples.utils.service.jpa.persistance.services.ActiveEntityService;
@@ -12,6 +13,7 @@ import bobkubista.examples.utils.service.jpa.persistance.services.ActiveEntitySe
  * @author Bob
  *
  */
+@Transactional
 public class GameService implements ActiveEntityService<MainGameEntity, Long> {
 
     @Inject
