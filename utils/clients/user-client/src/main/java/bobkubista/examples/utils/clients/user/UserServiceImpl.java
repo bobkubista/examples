bfd3a6b5c55c6684b9f2666c1978dc580b4d224d
Bob Kubista <bkubista@first8.nl>
2015-10-29 10:36:59 +0100

    refactored to make use of response statussen instead of ints

--- utils/clients/user-client/src/main/java/bobkubista/examples/utils/clients/user/UserServiceImpl.java
+++ utils/clients/user-client/src/main/java/bobkubista/examples/utils/clients/user/UserServiceImpl.java
@@ -5,6 +5,7 @@ package bobkubista.examples.utils.clients.user;
 
 import javax.inject.Inject;
 import javax.resource.spi.IllegalStateException;
+import javax.ws.rs.core.Response.Status;
 
 import bobkubista.examples.services.api.user.domain.User;
 import bobkubista.examples.services.api.user.domain.UserCollection;
@@ -22,9 +23,9 @@ public class UserServiceImpl extends AbstractActiveService<User, Long, UserColle
     @Override
     public boolean isAuthorized(final Long userId, final String right) throws IllegalStateException {
         final int status = this.getProxy().isAuthorized(userId, right).getStatus();
-        if (status == 200) {
+        if (status == Status.OK.getStatusCode()) {
             return true;
-        } else if (status == 401) {
+        } else if (status == Status.UNAUTHORIZED.getStatusCode()) {
             return false;
         } else {
             throw new IllegalStateException(String.format("Something went wrong in getting the right '%s' for user '%s'", right, userId));
