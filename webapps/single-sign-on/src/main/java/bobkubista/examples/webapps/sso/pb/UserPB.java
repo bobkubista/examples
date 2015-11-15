d3ec2cad95eca22e786d780584df57a3890fa010
Bob Kubista <bkubista@first8.nl>
2015-11-15 11:28:27 +0100

    sso

---
+++ webapps/single-sign-on/src/main/java/bobkubista/examples/webapps/sso/pb/UserPB.java
@@ -0,0 +1,13 @@
+package bobkubista.examples.webapps.sso.pb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class UserPB {
+
+    public void submit() {
+        return;
+    }
+}
