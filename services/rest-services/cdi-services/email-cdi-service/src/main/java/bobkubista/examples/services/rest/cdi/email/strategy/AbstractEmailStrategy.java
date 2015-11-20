d6ca186e8e069dfb5321d30dc135fe321726d666
Bob Kubista <bkubista@first8.nl>
2015-11-20 16:28:29 +0100

    fixing test and refactoring for loop to stream

--- services/rest-services/cdi-services/email-cdi-service/src/main/java/bobkubista/examples/services/rest/cdi/email/strategy/AbstractEmailStrategy.java
+++ services/rest-services/cdi-services/email-cdi-service/src/main/java/bobkubista/examples/services/rest/cdi/email/strategy/AbstractEmailStrategy.java
@@ -62,9 +62,7 @@ public abstract class AbstractEmailStrategy implements EmailStrategy {
         try {
             final String textToProcess = Resources.toString(url, Charsets.UTF_8);
             final VelocityContext context = new VelocityContext();
-            for (final Entry<String, ? extends Object> replacement : email.getReplacements().entrySet()) {
-                context.put(replacement.getKey(), replacement.getValue());
-            }
+            email.getReplacements().entrySet().forEach((final Entry<String, ? extends Object> replacement) -> context.put(replacement.getKey(), replacement.getValue()));
             final Writer swOut = new StringWriter();
             Velocity.evaluate(context, swOut, templateFile, textToProcess);
 
