4b7574ea3af2e11c1d54f64c5fe1e6b219e94808
Bob Kubista <bobbykubista@yahoo.com>
2015-12-05 12:39:26 +0100

    replaced maps impl

--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/CircuitBreakerRegistry.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/CircuitBreakerRegistry.java
@@ -3,32 +3,33 @@ package bobkubista.examples.utils.rest.utils.cirtuitbreaker;
 import java.time.Duration;
 import java.util.Map;
 
+import com.google.common.collect.Maps;
+
 import bobkubista.examples.utils.rest.utils.cirtuitbreaker.policiy.HealthPolicy;
-import jersey.repackaged.com.google.common.collect.Maps;
 
 public class CircuitBreakerRegistry {
 
-    private final Map<String, CircuitBreaker> circuitBreakerMap = Maps.newConcurrentMap();
+	private final Map<String, CircuitBreaker> circuitBreakerMap = Maps.newConcurrentMap();
 
-    private final HealthPolicy healthPolicy;
+	private final HealthPolicy healthPolicy;
 
-    private final int maxEntries = 100;
+	private final int maxEntries = 100;
 
-    public CircuitBreakerRegistry(final HealthPolicy healthPolicy) {
-        this.healthPolicy = healthPolicy;
-    }
+	public CircuitBreakerRegistry(final HealthPolicy healthPolicy) {
+		this.healthPolicy = healthPolicy;
+	}
 
-    public CircuitBreaker get(String scope) {
-        if (scope == null) {
-            scope = "__<NULL>__";
-        }
+	public CircuitBreaker get(String scope) {
+		if (scope == null) {
+			scope = "__<NULL>__";
+		}
 
-        CircuitBreaker circuitBreaker = this.circuitBreakerMap.get(scope);
-        if (circuitBreaker == null && this.circuitBreakerMap.size() < this.maxEntries) {
-            circuitBreaker = new CircuitBreaker(scope, this.healthPolicy, Duration.ofSeconds(3));
-            this.circuitBreakerMap.put(scope, circuitBreaker);
-        }
+		CircuitBreaker circuitBreaker = this.circuitBreakerMap.get(scope);
+		if (circuitBreaker == null && this.circuitBreakerMap.size() < this.maxEntries) {
+			circuitBreaker = new CircuitBreaker(scope, this.healthPolicy, Duration.ofSeconds(3));
+			this.circuitBreakerMap.put(scope, circuitBreaker);
+		}
 
-        return circuitBreaker;
-    }
+		return circuitBreaker;
+	}
 }
--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/MetricsRegistry.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/MetricsRegistry.java
@@ -4,8 +4,9 @@ import java.util.Map;
 
 import org.apache.commons.lang3.Validate;
 
+import com.google.common.collect.Maps;
+
 import bobkubista.examples.utils.rest.utils.cirtuitbreaker.transaction.TransactionMetrics;
-import jersey.repackaged.com.google.common.collect.Maps;
 
 /**
  *
@@ -14,23 +15,23 @@ import jersey.repackaged.com.google.common.collect.Maps;
  */
 public class MetricsRegistry {
 
-    private static final int BUFFERSIZE = 1000;
+	private static final int BUFFERSIZE = 1000;
 
-    private static final int MAX_ENTRIES = 100;
-    /**
-     * Key is the scope, aka the host. Value is a {@link TransactionMetrics}
-     */
-    private final Map<String, TransactionMetrics> metricsMap = Maps.newConcurrentMap();
+	private static final int MAX_ENTRIES = 100;
+	/**
+	 * Key is the scope, aka the host. Value is a {@link TransactionMetrics}
+	 */
+	private final Map<String, TransactionMetrics> metricsMap = Maps.newConcurrentMap();
 
-    public TransactionMetrics transactions(final String scope) {
-        Validate.notNull(scope);
+	public TransactionMetrics transactions(final String scope) {
+		Validate.notNull(scope);
 
-        TransactionMetrics metrics = this.metricsMap.get(scope);
-        if (metrics == null && this.metricsMap.size() < MAX_ENTRIES) {
-            metrics = new TransactionMetrics(BUFFERSIZE);
-            this.metricsMap.put(scope, metrics);
-        }
+		TransactionMetrics metrics = this.metricsMap.get(scope);
+		if (metrics == null && this.metricsMap.size() < MAX_ENTRIES) {
+			metrics = new TransactionMetrics(BUFFERSIZE);
+			this.metricsMap.put(scope, metrics);
+		}
 
-        return metrics;
-    }
+		return metrics;
+	}
 }
