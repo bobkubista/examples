dd8f4a0ce576e86277e3d7843660fc8f688567bb
Bob Kubista <bobbykubista@yahoo.com>
2015-12-06 21:17:22 +0100

    fixing compile error

--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/OpenState.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/OpenState.java
@@ -5,20 +5,19 @@ import java.time.Instant;
 import bobkubista.examples.utils.rest.utils.cirtuitbreaker.CircuitBreaker;
 
 final class OpenState implements CircuitBreakerState {
-    private final CircuitBreaker circuitBreaker;
+	private final CircuitBreaker circuitBreaker;
+	private final Instant exitDate;
 
-    /**
-     * Constructor
-     * @param circuitBreaker
-     */
-    OpenState(CircuitBreaker circuitBreaker) {
-        this.circuitBreaker = circuitBreaker;
-    }
+	/**
+	 * Constructor @param circuitBreaker
+	 */
+	OpenState(final CircuitBreaker circuitBreaker) {
+		this.circuitBreaker = circuitBreaker;
+		this.exitDate = Instant.now().plus(this.circuitBreaker.getOpenStateTimeout());
+	}
 
-    private final Instant exitDate = Instant.now().plus(this.circuitBreaker.getOpenStateTimeout());
-
-    @Override
-    public boolean isRequestAllowed() {
-        return Instant.now().isAfter(this.exitDate) ? this.circuitBreaker.changeState(new HalfOpenState(this.circuitBreaker)).isRequestAllowed() : false;
-    }
+	@Override
+	public boolean isRequestAllowed() {
+		return Instant.now().isAfter(this.exitDate) ? this.circuitBreaker.changeState(new HalfOpenState(this.circuitBreaker)).isRequestAllowed() : false;
+	}
 }
