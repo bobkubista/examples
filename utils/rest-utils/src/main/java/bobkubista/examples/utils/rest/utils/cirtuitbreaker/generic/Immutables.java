ee25c4f0db2a490fe2e846d52deb8256828c767d
Bob Kubista <bkubista@first8.nl>
2015-12-08 12:02:31 +0100

    javadoc

--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/generic/Immutables.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/generic/Immutables.java
@@ -1,10 +1,9 @@
 package bobkubista.examples.utils.rest.utils.cirtuitbreaker.generic;
 
 import java.util.List;
-
 import java.util.Map;
-import java.util.Set;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -13,47 +12,103 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
-
-
+/**
+ * Transformer class to build immutable collections
+ *
+ * @author Bob
+ *
+ */
 public class Immutables {
 
+    /**
+     *
+     * @return immutable {@link Collector} to {@link List}
+     */
     public static <T> Collector<T, ?, ImmutableList<T>> toList() {
-        Collector<T, ?, List<T>> collector = Collectors.toList();
+        final Collector<T, ?, List<T>> collector = Collectors.toList();
         return Collectors.collectingAndThen(collector, ImmutableList::copyOf);
     }
-    
-    public static <T> Collector<T, ?, ImmutableSet<T>> toSet() {
-        Collector<T, ?, List<T>> collector = Collectors.toList();
-        return Collectors.collectingAndThen(collector, ImmutableSet::copyOf);
-    }
-        
-    
-    public static <T, K, U> Collector<T, ?, ImmutableMap<K,U>> toMap(Function<? super T, ? extends K> keyMapper,
-                                                                    Function<? super T, ? extends U> valueMapper) {
-        Collector<T, ?, Map<K,U>> collector = Collectors.toMap(keyMapper, valueMapper);
+
+    /**
+     *
+     * @return immutable {@link Collector} to {@link Map}
+     */
+    public static <T, K, U> Collector<T, ?, ImmutableMap<K, U>> toMap(final Function<? super T, ? extends K> keyMapper, final Function<? super T, ? extends U> valueMapper) {
+        final Collector<T, ?, Map<K, U>> collector = Collectors.toMap(keyMapper, valueMapper);
         return Collectors.collectingAndThen(collector, ImmutableMap::copyOf);
     }
-    
-    public static <F, T> ImmutableSet<T> transform(Set<F> fromList, Function<? super F, ? extends T> function) {   
-        return fromList.stream().map(function).collect(toSet());
-    }
-    
-    public static <F, T> ImmutableList<T> transform(List<F> fromList, Function<? super F, ? extends T> function) {   
-        return fromList.stream().map(function).collect(toList());
-    }
-    
-    public static <K, V1, V2> ImmutableMap<K, V2> transformValues(Map<K, V1> fromMap, Function<? super V1, V2> function) {
-        Collector<Entry<K, V1>, ?, ImmutableMap<K, V2>> collector = toMap(Entry::getKey, entry -> function.apply(entry.getValue()));
-        return fromMap.entrySet().stream().collect(collector);    
+
+    /**
+     *
+     * @return immutable {@link Collector} to {@link Set}
+     */
+    public static <T> Collector<T, ?, ImmutableSet<T>> toSet() {
+        final Collector<T, ?, List<T>> collector = Collectors.toList();
+        return Collectors.collectingAndThen(collector, ImmutableSet::copyOf);
     }
 
-    public static <K1, K2, V> ImmutableMap<K2, V> transformKeys(Map<K1, V> fromMap, Function<? super K1, K2> function) {
-        Collector<Entry<K1, V>, ?, ImmutableMap<K2, V>> collector = toMap(entry -> function.apply(entry.getKey()), Entry::getValue);
-        return fromMap.entrySet().stream().collect(collector);    
+    /**
+     * @param fromList
+     *            the input {@link List}
+     * @param function
+     *            the {@link Function} to transform the {@link List}
+     * @return transform lists
+     */
+    public static <F, T> ImmutableList<T> transform(final List<F> fromList, final Function<? super F, ? extends T> function) {
+        return fromList.stream().map(function).collect(toList());
     }
-    
-    public static <K1, K2, V1, V2> ImmutableMap<K2, V2> transform(Map<K1, V1> fromMap, Function<? super K1, K2> keyFunction, Function<? super V1, V2> valueFunction) {
-        Collector<Entry<K1, V1>, ?, ImmutableMap<K2, V2>> collector = toMap(entry -> keyFunction.apply(entry.getKey()), entry -> valueFunction.apply(entry.getValue()));
-        return fromMap.entrySet().stream().collect(collector);    
+
+    /**
+     *
+     * @param fromMap
+     *            the input {@link Map}
+     * @param keyFunction
+     *            the {@link Function} to generate the keys
+     * @param valueFunction
+     *            the {@link Function} to generate the values
+     * @return an immutable {@link Map}
+     */
+    public static <K1, K2, V1, V2> ImmutableMap<K2, V2> transform(final Map<K1, V1> fromMap, final Function<? super K1, K2> keyFunction,
+            final Function<? super V1, V2> valueFunction) {
+        final Collector<Entry<K1, V1>, ?, ImmutableMap<K2, V2>> collector = toMap(entry -> keyFunction.apply(entry.getKey()), entry -> valueFunction.apply(entry.getValue()));
+        return fromMap.entrySet().stream().collect(collector);
+    }
+
+    /**
+     *
+     * @param fromList
+     *            the input {@link List}
+     * @param function
+     *            the {@link Function} to transform to a {@link Set}
+     * @return an immutable {@link Set}
+     */
+    public static <F, T> ImmutableSet<T> transform(final Set<F> fromList, final Function<? super F, ? extends T> function) {
+        return fromList.stream().map(function).collect(toSet());
+    }
+
+    /**
+     *
+     * @param fromMap
+     *            the input {@link Map}
+     * @param function
+     *            the function to transform to a {@link Map}
+     * @return an immutable {@link Map}
+     */
+    public static <K1, K2, V> ImmutableMap<K2, V> transformKeys(final Map<K1, V> fromMap, final Function<? super K1, K2> function) {
+        final Collector<Entry<K1, V>, ?, ImmutableMap<K2, V>> collector = toMap(entry -> function.apply(entry.getKey()), Entry::getValue);
+        return fromMap.entrySet().stream().collect(collector);
+    }
+
+    /**
+     *
+     * @param fromMap
+     *            the input {@link Map}
+     * @param function
+     *            the function to transform to a {@link Map}
+     * @return an immutable {@link Map}
+     */
+    public static <K, V1, V2> ImmutableMap<K, V2> transformValues(final Map<K, V1> fromMap, final Function<? super V1, V2> function) {
+        final Collector<Entry<K, V1>, ?, ImmutableMap<K, V2>> collector = toMap(Entry::getKey, entry -> function.apply(entry.getValue()));
+        return fromMap.entrySet().stream().collect(collector);
     }
 }
