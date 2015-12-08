d591d1a9aa76e6d1890c932438ef3f53db846139
Bob Kubista <bkubista@first8.nl>
2015-12-08 12:41:26 +0100

    javadoc

--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/CircuitBreakerState.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/CircuitBreakerState.java
@@ -1,5 +1,17 @@
 package bobkubista.examples.utils.rest.utils.cirtuitbreaker.state;
 
+/**
+ * A state of a circuitbreaker
+ * 
+ * @author Bob
+ *
+ */
+@FunctionalInterface
 public interface CircuitBreakerState {
+
+    /**
+     *
+     * @return is a request allowed?
+     */
     boolean isRequestAllowed();
 }
