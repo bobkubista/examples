3fcb4daaace7d3fd3b4a4e1a39fd7c590a45c13c
Bob Kubista <bkubista@first8.nl>
2015-12-16 14:37:01 +0100

    adding tests

--- utils/rest-utils/src/test/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/ClosedStateTest.java
+++ utils/rest-utils/src/test/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/ClosedStateTest.java
@@ -1,21 +1,46 @@
 package bobkubista.examples.utils.rest.utils.cirtuitbreaker.state;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
 
-import org.junit.Ignore;
+import java.time.Duration;
+
 import org.junit.Test;
+import org.mockito.Matchers;
 
-@Ignore
+import bobkubista.examples.utils.rest.utils.cirtuitbreaker.CircuitBreaker;
+import bobkubista.examples.utils.rest.utils.cirtuitbreaker.policiy.HealthPolicy;
+
 public class ClosedStateTest {
 
     @Test
-    public void testClosedState() {
-        fail("Not yet implemented");
+    public void testIsRequestAllowed() {
+        final HealthPolicy mockPolicy = mock(HealthPolicy.class);
+        when(mockPolicy.isHealthy(Matchers.anyString())).thenReturn(true);
+
+        final CircuitBreaker mockCircuitBreaker = mock(CircuitBreaker.class);
+        when(mockCircuitBreaker.getPolicy()).thenReturn(mockPolicy);
+        when(mockCircuitBreaker.getScope()).thenReturn("host");
+
+        final ClosedState state = new ClosedState();
+        assertTrue(state.isRequestAllowed(mockCircuitBreaker));
     }
 
     @Test
-    public void testIsRequestAllowed() {
-        fail("Not yet implemented");
+    public void testIsRequestAllowedNo() {
+        final HealthPolicy mockPolicy = mock(HealthPolicy.class);
+        when(mockPolicy.isHealthy(Matchers.anyString())).thenReturn(false);
+
+        final CircuitBreaker mockCircuitBreaker = mock(CircuitBreaker.class);
+        when(mockCircuitBreaker.getPolicy()).thenReturn(mockPolicy);
+        when(mockCircuitBreaker.getScope()).thenReturn("host");
+        when(mockCircuitBreaker.getOpenStateTimeout()).thenReturn(Duration.ofDays(1));
+        when(mockCircuitBreaker.changeState(Matchers.any(OpenState.class))).thenReturn(new OpenState(Duration.ofDays(1)));
+
+        final ClosedState state = new ClosedState();
+        assertFalse(state.isRequestAllowed(mockCircuitBreaker));
     }
 
 }
--- utils/rest-utils/src/test/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/OpenStateTest.java
+++ utils/rest-utils/src/test/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/OpenStateTest.java
@@ -1,21 +1,51 @@
 package bobkubista.examples.utils.rest.utils.cirtuitbreaker.state;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
 
-import org.junit.Ignore;
+import java.time.Duration;
+
 import org.junit.Test;
+import org.mockito.Matchers;
 
-@Ignore
+import bobkubista.examples.utils.rest.utils.cirtuitbreaker.CircuitBreaker;
+import bobkubista.examples.utils.rest.utils.cirtuitbreaker.policiy.HealthPolicy;
+
 public class OpenStateTest {
 
     @Test
-    public void testIsRequestAllowed() {
-        fail("Not yet implemented");
+    public void testIsRequestAllowed() throws InterruptedException {
+        final HealthPolicy mockPolicy = mock(HealthPolicy.class);
+        when(mockPolicy.isHealthy(Matchers.anyString())).thenReturn(true);
+
+        final CircuitBreakerState mockHalfOpenState = mock(CircuitBreakerState.class);
+        when(mockHalfOpenState.isRequestAllowed(Matchers.any(CircuitBreaker.class))).thenReturn(true);
+
+        final CircuitBreaker mockCircuitBreaker = mock(CircuitBreaker.class);
+        when(mockCircuitBreaker.getPolicy()).thenReturn(mockPolicy);
+        when(mockCircuitBreaker.getScope()).thenReturn("host");
+        when(mockCircuitBreaker.changeState(Matchers.any(CircuitBreakerState.class))).thenReturn(mockHalfOpenState);
+
+        final OpenState state = new OpenState(Duration.ofMillis(1));
+        Thread.sleep(2L);
+        assertTrue(state.isRequestAllowed(mockCircuitBreaker));
     }
 
     @Test
-    public void testOpenState() {
-        fail("Not yet implemented");
+    public void testIsRequestAllowedNo() {
+        final HealthPolicy mockPolicy = mock(HealthPolicy.class);
+        when(mockPolicy.isHealthy(Matchers.anyString())).thenReturn(false);
+
+        final CircuitBreaker mockCircuitBreaker = mock(CircuitBreaker.class);
+        when(mockCircuitBreaker.getPolicy()).thenReturn(mockPolicy);
+        when(mockCircuitBreaker.getScope()).thenReturn("host");
+        when(mockCircuitBreaker.getOpenStateTimeout()).thenReturn(Duration.ofDays(1));
+        when(mockCircuitBreaker.changeState(Matchers.any(OpenState.class))).thenReturn(new OpenState(Duration.ofDays(1)));
+
+        final OpenState state = new OpenState(Duration.ofDays(1));
+        assertFalse(state.isRequestAllowed(mockCircuitBreaker));
     }
 
 }
