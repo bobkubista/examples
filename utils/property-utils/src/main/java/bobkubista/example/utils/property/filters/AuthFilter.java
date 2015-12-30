eb4d416f8b88c5ef2529d2685ed4aa583cf5f5d8
Bob Kubista <bkubista@first8.nl>
2015-12-30 16:27:47 +0100

    fixing some things adding authentication filter adding first jmeter test

---
+++ utils/property-utils/src/main/java/bobkubista/example/utils/property/filters/AuthFilter.java
@@ -0,0 +1,78 @@
+/**
+ * Bob Kubista's examples
+ */
+package bobkubista.example.utils.property.filters;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.annotation.Priority;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang.StringUtils;
+
+import bobkubista.example.utils.property.ServerProperties;
+
+/**
+ * @author Bob
+ *
+ */
+@PreMatching
+@Priority(Priorities.AUTHENTICATION)
+public class AuthFilter implements ContainerRequestFilter {
+
+    @Context
+    private HttpServletRequest servletRequest;
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext) throws IOException {
+        final String httpMethod = requestContext.getMethod();
+        if ("OPTIONS".equalsIgnoreCase(httpMethod)) {
+            return;
+        }
+        final String authenticationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+        if (authenticationHeader == null) {
+            throw new NotAuthorizedException("Bearer");
+        }
+
+        final String token = this.parseToken(authenticationHeader);
+        final Principal user = this.getUserByToken(token);
+
+        if (!this.verifyToken(user)) {
+            throw new NotAuthorizedException("Bearer error=\"invalid_token\"");
+        }
+        // requestContext.setSecurityContext(new TimSecurityContext(user,
+        // servletRequest));
+
+    }
+
+    private Principal getUserByToken(final String token) {
+        final String casUrl = ServerProperties.getString("cas.url");
+        try {
+            ClientBuilder.newClient().target(casUrl).queryParam("access_token", token).request(MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML).get();
+
+            // Principal principal = response.readEntity(CasUser.class);
+            return null;
+        } catch (final Exception e) {
+            return null;
+        }
+    }
+
+    private String parseToken(final String authenticationHeader) {
+        return StringUtils.stripStart(authenticationHeader, "Bearer ");
+    }
+
+    private boolean verifyToken(final Principal principal) {
+        return principal != null;
+    }
+
+}
