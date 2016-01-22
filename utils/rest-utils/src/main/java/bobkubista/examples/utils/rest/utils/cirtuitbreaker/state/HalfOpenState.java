faa1edb346f74b34c106d749a213071312834455
Bob Kubista <bobbykubista@yahoo.com>
2016-01-22 14:34:37 +0100

    static

--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/HalfOpenState.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/cirtuitbreaker/state/HalfOpenState.java
@@ -3,23 +3,23 @@ package bobkubista.examples.utils.rest.utils.cirtuitbreaker.state;
 import bobkubista.examples.utils.rest.utils.cirtuitbreaker.CircuitBreaker;
 
 final class HalfOpenState implements CircuitBreakerState {
-    private final double chance = 0.02; // 2% will be passed through
+	private static final double chance = 0.02; // 2% will be passed through
 
-    /**
-     * Constructor
-     *
-     * @param circuitBreaker
-     */
-    HalfOpenState() {
-        super();
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param circuitBreaker
+	 */
+	HalfOpenState() {
+		super();
+	}
 
-    @Override
-    public boolean isRequestAllowed(final CircuitBreaker circuitBreaker) {
-        if (circuitBreaker.getPolicy().isHealthy(circuitBreaker.getScope())) {
-            return circuitBreaker.changeState(new ClosedState()).isRequestAllowed(circuitBreaker);
-        } else {
-            return circuitBreaker.getRandom().nextDouble() <= this.chance;
-        }
-    }
+	@Override
+	public boolean isRequestAllowed(final CircuitBreaker circuitBreaker) {
+		if (circuitBreaker.getPolicy().isHealthy(circuitBreaker.getScope())) {
+			return circuitBreaker.changeState(new ClosedState()).isRequestAllowed(circuitBreaker);
+		} else {
+			return circuitBreaker.getRandom().nextDouble() <= HalfOpenState.chance;
+		}
+	}
 }
