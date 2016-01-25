b9ff2fd642853d143c52db424c66a6df6a60e0f3
Bob Kubista <bkubista@first8.nl>
2016-01-25 23:24:35 +0100

    fix tests

--- utils/service-utils/jpa-persistance/src/main/java/bobkubista/examples/utils/service/jpa/persistance/services/IdentifiableEntityService.java
+++ utils/service-utils/jpa-persistance/src/main/java/bobkubista/examples/utils/service/jpa/persistance/services/IdentifiableEntityService.java
@@ -3,6 +3,7 @@ package bobkubista.examples.utils.service.jpa.persistance.services;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,83 +24,91 @@ import bobkubista.examples.utils.service.jpa.persistance.entity.AbstractIdentifi
 @FunctionalInterface
 public interface IdentifiableEntityService<TYPE extends AbstractIdentifiableEntity<ID>, ID extends Serializable> {
 
-	/**
-	 * Check if object of {@link AbstractIdentifiableEntity} exists
-	 *
-	 * @param object
-	 *            the object to check
-	 * @return true if exists
-	 */
-	public default boolean contains(final TYPE object) {
-		return this.getDAO().contains(object);
-	}
+    /**
+     * Check if object of {@link AbstractIdentifiableEntity} exists
+     *
+     * @param object
+     *            the object to check
+     * @return true if exists
+     */
+    public default boolean contains(final TYPE object) {
+        return this.getDAO()
+                .contains(object);
+    }
 
-	/**
-	 * Create the object of {@link AbstractIdentifiableEntity} type
-	 *
-	 * @param object
-	 *            the object to create
-	 * @return <code>TYPE</code> that was created
-	 */
-	public default TYPE create(final TYPE object) {
-		this.getDAO().create(object);
-		return this.getDAO().getById(object.getId());
-	}
+    /**
+     * Create the object of {@link AbstractIdentifiableEntity} type
+     *
+     * @param object
+     *            the object to create
+     * @return <code>TYPE</code> that was created
+     */
+    public default TYPE create(final TYPE object) {
+        this.getDAO()
+                .create(object);
+        return this.getDAO()
+                .getById(object.getId());
+    }
 
-	/**
-	 * delete the specific object of {@link AbstractIdentifiableEntity}
-	 *
-	 * @param object
-	 *            the object to delete
-	 */
-	public default void delete(final TYPE object) {
-		this.getDAO().delete(object);
-	}
+    /**
+     * delete the specific object of {@link AbstractIdentifiableEntity}
+     *
+     * @param object
+     *            the object to delete
+     */
+    public default void delete(final TYPE object) {
+        this.getDAO()
+                .delete(object);
+    }
 
-	/**
-	 * get all known {@link AbstractIdentifiableEntity} of that type
-	 *
-	 * @param sortFields
-	 *            which field to sort
-	 * @param page
-	 *            the page
-	 * @param maxResults
-	 *            the max amount of results per page
-	 * @return a {@link Collection} of {@link AbstractIdentifiableEntity} of the
-	 *         same type
-	 */
-	public default Collection<TYPE> getAll(final List<String> sortFields, final int page, final int maxResults) {
-		return this.getDAO().getAll(sortFields, page, maxResults, null);
-	}
+    /**
+     * get all known {@link AbstractIdentifiableEntity} of that type
+     *
+     * @param sortFields
+     *            which field to sort
+     * @param page
+     *            the page
+     * @param maxResults
+     *            the max amount of results per page
+     * @return a {@link Collection} of {@link AbstractIdentifiableEntity} of the
+     *         same type
+     */
+    public default Collection<TYPE> getAll(final List<String> sortFields, final int page, final int maxResults) {
+        return this.getDAO()
+                .getAll(sortFields, page, maxResults, Optional.empty());
+    }
 
-	/**
-	 * get the {@link AbstractIdentifiableEntity}
-	 *
-	 * @param identifier
-	 *            the identfier
-	 * @return the {@link AbstractIdentifiableEntity}
-	 */
-	public default TYPE getById(final ID identifier) {
-		return this.getDAO().getById(identifier);
-	}
+    /**
+     * get the {@link AbstractIdentifiableEntity}
+     *
+     * @param identifier
+     *            the identfier
+     * @return the {@link AbstractIdentifiableEntity}
+     */
+    public default TYPE getById(final ID identifier) {
+        return this.getDAO()
+                .getById(identifier);
+    }
 
-	/**
-	 * update the object of {@link AbstractIdentifiableEntity}
-	 *
-	 * @param object
-	 *            the object to update
-	 * @return the updated object
-	 */
-	public default TYPE update(final TYPE object) {
-		this.getDAO().update(object);
-		return this.getDAO().getById(object.getId());
-	}
+    /**
+     * update the object of {@link AbstractIdentifiableEntity}
+     *
+     * @param object
+     *            the object to update
+     * @return the updated object
+     */
+    public default TYPE update(final TYPE object) {
+        this.getDAO()
+                .update(object);
+        return this.getDAO()
+                .getById(object.getId());
+    }
 
-	/**
-	 * Get the {@link GenericDao}
-	 *
-	 * @return A subtype of {@link GenericDao}
-	 */
-	GenericDao<TYPE, ID> getDAO();
+    /**
+     * Get the {@link GenericDao}
+     *
+     * @return A subtype of {@link GenericDao}
+     */
+    GenericDao<TYPE, ID> getDAO();
 
 }
