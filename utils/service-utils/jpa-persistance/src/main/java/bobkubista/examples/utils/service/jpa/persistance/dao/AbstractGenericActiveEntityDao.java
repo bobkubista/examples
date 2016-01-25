65b09c7f76e66599c013eb20e2027916924477c8
Bob Kubista <bkubista@first8.nl>
2016-01-25 22:20:08 +0100

    Revert "less code"
    This reverts commit 9ca26d33aa6b9e9bdcb3f9dde4dc796c1fd2346b.

--- utils/service-utils/jpa-persistance/src/main/java/bobkubista/examples/utils/service/jpa/persistance/dao/AbstractGenericActiveEntityDao.java
+++ utils/service-utils/jpa-persistance/src/main/java/bobkubista/examples/utils/service/jpa/persistance/dao/AbstractGenericActiveEntityDao.java
@@ -7,6 +7,11 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import bobkubista.examples.utils.service.jpa.persistance.entity.AbstractGenericActiveEntity;
 
@@ -23,6 +28,7 @@ public abstract class AbstractGenericActiveEntityDao<TYPE extends AbstractGeneri
 
     @Override
     public Collection<TYPE> findAllActive(final List<String> sortFields, final Integer page, final Integer maxResults) {
-        return this.getAll(sortFields, page, maxResults, Optional.of((root, build) -> build.equal(root.get("active"), true)));
+        final Optional<BiFunction<Root<TYPE>, CriteriaBuilder, Predicate>> optionalWhere = Optional.of((root, build) -> build.equal(root.get("active"), true));
+        return this.getAll(sortFields, page, maxResults, optionalWhere);
     }
 }
