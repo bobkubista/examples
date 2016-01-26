f02fad9095f4d890560337f9517a55ae26da06bd
Bob Kubista <bkubista@first8.nl>
2016-01-26 13:56:03 +0100

    javadoc

--- utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/service/ActiveService.java
+++ utils/rest-utils/src/main/java/bobkubista/examples/utils/rest/utils/service/ActiveService.java
@@ -25,8 +25,8 @@ public interface ActiveService<TYPE extends AbstractGenericActiveDomainObject<ID
      *            which fields to sort
      * @param page
      *            how many to skip: page * maxResults
-     * @param how
-     *            many results to give back
+     * @param maxResults
+     *            how many results to give back
      * @return Get all active {@link AbstractGenericActiveDomainObject}
      */
     Collection<TYPE> getAllActive(final List<String> sort, final Integer page, final Integer maxResults);
@@ -36,8 +36,8 @@ public interface ActiveService<TYPE extends AbstractGenericActiveDomainObject<ID
      *            which fields to sort
      * @param page
      *            how many to skip: page * maxResults
-     * @param how
-     *            many results to give back
+     * @param maxResults
+     *            how many results to give back
      * @return Get all active {@link AbstractGenericActiveDomainObject}
      */
     CompletableFuture<Collection<TYPE>> getAllActiveASync(final List<String> sort, final Integer page, final Integer maxResults);
