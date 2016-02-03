1d62f5176b3bdc48f21c128cd43d86507b2d0e2c
Bob Kubista <bkubista@first8.nl>
2016-02-03 11:14:16 +0100

    fixed tests

--- utils/domain-model/src/test/java/bobkubista/examples/utils/domain/model/domainmodel/validators/CollectionReducerTest.java
+++ utils/domain-model/src/test/java/bobkubista/examples/utils/domain/model/domainmodel/validators/CollectionReducerTest.java
@@ -6,13 +6,15 @@ package bobkubista.examples.utils.domain.model.domainmodel.validators;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 
 import bobkubista.examples.utils.domain.model.domainmodel.identification.AbstractGenericFunctionalIdentifiableDomainObject;
 import bobkubista.examples.utils.domain.model.domainmodel.identification.GenericTestActiveDomainObject;
+import bobkubista.examples.utils.domain.model.domainmodel.identification.GenericTestFunctionalDomainObject;
 
 /**
  * @author Bob
@@ -20,17 +22,16 @@ import bobkubista.examples.utils.domain.model.domainmodel.identification.Generic
  */
 public class CollectionReducerTest {
 
-    @Ignore
     @Test(expected = IllegalStateException.class)
     public void testReducerException() {
         final Stream<AbstractGenericFunctionalIdentifiableDomainObject<Integer>> stream = Stream.of(new GenericTestActiveDomainObject(), new GenericTestActiveDomainObject());
-        CollectionReducer.findOnlyOne("1", stream, t -> t.getId(), IllegalStateException::new);
+        CollectionReducer.findOnlyOne(1, stream, t -> t.getId(), IllegalStateException::new);
     }
 
     @Test
     public void testReducerOkay() {
-        final List<AbstractGenericFunctionalIdentifiableDomainObject<Integer>> stream = new ArrayList<>(
-                Arrays.asList(new GenericTestActiveDomainObject(), new GenericTestActiveDomainObject()));
-        CollectionReducer.findOnlyOne("1", stream.stream(), t -> t.getId(), IllegalStateException::new);
+        final GenericTestActiveDomainObject result = new GenericTestActiveDomainObject();
+        final List<AbstractGenericFunctionalIdentifiableDomainObject<Integer>> stream = new ArrayList<>(Arrays.asList(result, new GenericTestFunctionalDomainObject()));
+        Assert.assertEquals(Optional.of(result), CollectionReducer.findOnlyOne(1, stream.stream(), t -> t.getId(), IllegalStateException::new));
     }
 }
