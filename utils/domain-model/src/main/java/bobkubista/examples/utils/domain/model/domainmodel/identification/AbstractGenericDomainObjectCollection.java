36fb8751d4b8115f489fd7c85e2b6be3584957f9
Bob Kubista <bkubista@first8.nl>
2016-02-04 16:43:44 +0100

    adding possibilities for links HATEOAS

--- utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/domainmodel/identification/AbstractGenericDomainObjectCollection.java
+++ utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/domainmodel/identification/AbstractGenericDomainObjectCollection.java
@@ -4,11 +4,14 @@
 package bobkubista.examples.utils.domain.model.domainmodel.identification;
 
 import java.util.Collection;
+import java.util.List;
 
+import javax.ws.rs.core.Link;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
  * @author bkubista
@@ -24,6 +27,10 @@ public abstract class AbstractGenericDomainObjectCollection<TYPE extends DomainO
 
     @XmlElement
     private long count;
+
+    @XmlElement(name = "link")
+    @XmlJavaTypeAdapter(Link.JaxbAdapter.class)
+    private List<Link> links;
 
     /**
      * Constructor
@@ -46,12 +53,27 @@ public abstract class AbstractGenericDomainObjectCollection<TYPE extends DomainO
     public abstract Collection<TYPE> getDomainCollection();
 
     /**
+     * @return the links
+     */
+    public final List<Link> getLinks() {
+        return this.links;
+    }
+
+    /**
      *
      * @param amount
      *            count
      */
     public void setAmount(final long amount) {
         this.count = amount;
+    }
+
+    /**
+     * @param links
+     *            the links to set
+     */
+    public final void setLinks(final List<Link> links) {
+        this.links = links;
     }
 
 }
