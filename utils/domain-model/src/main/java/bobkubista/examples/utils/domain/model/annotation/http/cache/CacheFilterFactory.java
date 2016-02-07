9d463d3382367969b7ddd059b62eb0e4014f0993
Bob Kubista <bkubista@first8.nl>
2016-02-07 11:46:13 +0100

    added todo, multiple annotations doesn't work

--- utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/annotation/http/cache/CacheFilterFactory.java
+++ utils/domain-model/src/main/java/bobkubista/examples/utils/domain/model/annotation/http/cache/CacheFilterFactory.java
@@ -2,6 +2,8 @@
 package bobkubista.examples.utils.domain.model.annotation.http.cache;
 
 import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -31,18 +33,18 @@ public class CacheFilterFactory implements DynamicFeature {
     @Override
     public void configure(final ResourceInfo resourceInfo, final FeatureContext featureContext) {
 
-        this.setHeader(resourceInfo, featureContext, CacheNo.class, t -> CacheNo.HEADER);
-        this.setHeader(resourceInfo, featureContext, CacheMustRevalidate.class, t -> CacheMustRevalidate.HEADER);
-        this.setHeader(resourceInfo, featureContext, CacheNoStore.class, t -> CacheNoStore.HEADER);
-        this.setHeader(resourceInfo, featureContext, CacheNoTransform.class, t -> CacheNoTransform.HEADER);
-        this.setHeader(resourceInfo, featureContext, CachePrivate.class, t -> CachePrivate.HEADER);
-        this.setHeader(resourceInfo, featureContext, CacheProxyRevalidate.class, t -> CacheProxyRevalidate.HEADER);
-        this.setHeader(resourceInfo, featureContext, CachePublic.class, t -> CachePublic.HEADER);
+        this.setHeader(resourceInfo, featureContext, CacheNo.class, t -> Collections.singletonList(CacheNo.HEADER));
+        this.setHeader(resourceInfo, featureContext, CacheMustRevalidate.class, t -> Collections.singletonList(CacheMustRevalidate.HEADER));
+        this.setHeader(resourceInfo, featureContext, CacheNoStore.class, t -> Collections.singletonList(CacheNoStore.HEADER));
+        this.setHeader(resourceInfo, featureContext, CacheNoTransform.class, t -> Collections.singletonList(CacheNoTransform.HEADER));
+        this.setHeader(resourceInfo, featureContext, CachePrivate.class, t -> Collections.singletonList(CachePrivate.HEADER));
+        this.setHeader(resourceInfo, featureContext, CacheProxyRevalidate.class, t -> Collections.singletonList(CacheProxyRevalidate.HEADER));
+        this.setHeader(resourceInfo, featureContext, CachePublic.class, t -> Collections.singletonList(CachePublic.HEADER));
 
-        this.setHeader(resourceInfo, featureContext, CacheSMaxAge.class, maxAge -> CacheSMaxAge.HEADER + Long.toString(maxAge.unit()
-                .toSeconds(maxAge.time())));
-        this.setHeader(resourceInfo, featureContext, CacheMaxAge.class, maxAge -> CacheMaxAge.HEADER + Long.toString(maxAge.unit()
-                .toSeconds(maxAge.time())));
+        this.setHeader(resourceInfo, featureContext, CacheSMaxAge.class, maxAge -> Collections.singletonList(CacheSMaxAge.HEADER + Long.toString(maxAge.unit()
+                .toSeconds(maxAge.time()))));
+        this.setHeader(resourceInfo, featureContext, CacheMaxAge.class, maxAge -> Collections.singletonList(CacheMaxAge.HEADER + Long.toString(maxAge.unit()
+                .toSeconds(maxAge.time()))));
     }
 
     /**
@@ -58,14 +60,17 @@ public class CacheFilterFactory implements DynamicFeature {
      *            {@link Function} of the cache annotation to {@link String}
      */
     protected <T extends Annotation> void setHeader(final ResourceInfo resourceInfo, final FeatureContext featureContext, final Class<T> annotationClass,
-            final Function<T, String> header) {
+            final Function<T, List<Object>> header) {
         if (resourceInfo.getResourceMethod()
                 .isAnnotationPresent(annotationClass)) {
             final T annotation = resourceInfo.getResourceMethod()
                     .getDeclaredAnnotation(annotationClass);
+            // TODO only process one annotation. This is not correct
             featureContext.register((ContainerResponseFilter) (requestContext, responseContext) -> responseContext.getHeaders()
-                    .add(HttpHeaders.CACHE_CONTROL, header.apply(annotation)));
+                    .merge(HttpHeaders.CACHE_CONTROL, header.apply(annotation), (t, u) -> {
+                        t.addAll(u);
+                        return t;
+                    }));
         }
-
     }
 }
