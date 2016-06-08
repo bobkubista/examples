53d1e6469538a342bc299dca39cb5fa63c22ada5
Bob Kubista <bkubista@first8.nl>
2016-06-08 13:07:53 +0200

    Revert "Revert "updating hibernate""
    This reverts commit 074036010b7e1691d80f3e8658e5ad241b0c8219.

--- utils/service-utils/jpa-persistance/src/main/java/bobkubista/examples/utils/service/jpa/persistance/FlywayIntegrator.java
+++ utils/service-utils/jpa-persistance/src/main/java/bobkubista/examples/utils/service/jpa/persistance/FlywayIntegrator.java
@@ -5,10 +5,9 @@ import java.util.Map;
 
 import org.apache.commons.lang3.CharEncoding;
 import org.flywaydb.core.Flyway;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.integrator.spi.Integrator;
-import org.hibernate.metamodel.source.MetadataImplementor;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +19,7 @@ import bobkubista.example.utils.property.ServerProperties;
  *
  */
 public class FlywayIntegrator implements Integrator {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(FlywayIntegrator.class);
 
     @Override
@@ -29,8 +29,7 @@ public class FlywayIntegrator implements Integrator {
     }
 
     @Override
-    public void integrate(final Configuration configuration, final SessionFactoryImplementor sessionFactory, final SessionFactoryServiceRegistry serviceRegistry) {
-
+    public void integrate(final Metadata metadata, final SessionFactoryImplementor sessionFactory, final SessionFactoryServiceRegistry serviceRegistry) {
         // Create the Flyway instance
         final Flyway flyway = new Flyway();
 
@@ -55,11 +54,4 @@ public class FlywayIntegrator implements Integrator {
         flyway.validate();
         LOGGER.info("Finished database validation");
     }
-
-    @Override
-    public void integrate(final MetadataImplementor metadata, final SessionFactoryImplementor sessionFactory, final SessionFactoryServiceRegistry serviceRegistry) {
-        // not needed
-
-    }
-
 }
